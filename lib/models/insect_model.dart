261d55f0813013111500c405dcbe517e90f91b28
Abdul Muksith <106472158+MuksithMRA@users.noreply.github.com>
2023-04-12 02:11:40 +0530

    updated insect model

--- lib/models/insect_model.dart
+++ lib/models/insect_model.dart
@@ -2,21 +2,25 @@ class InsectModel {
   int id;
   String name;
   String description;
+  String image;
 
   InsectModel({
     this.id = 0,
     this.name = '',
     this.description = '',
+    this.image = '',
   });
 
   InsectModel.fromJson(Map<String, dynamic> json)
       : id = json['id'] ?? 0,
         name = json['name'] ?? '',
-        description = json['description'] ?? '';
+        description = json['description'] ?? '',
+        image = json['image'] ?? '';
 
   Map<String, dynamic> toJson() => {
         'id': id,
         'name': name,
         'description': description,
+        'image': image,
       };
 }
